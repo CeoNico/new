11e9de7e6808b7f32b8746a323301c4bc5692c02
CeoNico <2004ssnicoo@gmail.com>
2024-04-11 12:11:29 -0400

    Basic Setup

---
+++ src/app/Dashboard/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='h-screen w-full border-2'>
+        <div>
+            {/*Component Container */}
+            {/*Component To Create
+                1.Total Sales
+                2.Total Revenue
+                3.Total Product Sales
+                4.Best Selling Product */}
+        </div>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ src/lib/actions.js
@@ -0,0 +1,12 @@
+'use server'
+
+
+export async function getTotalSales() {
+    {/*Create Logic for total sales */}
+}
+export async function getBestSeller() {
+    {/*Create Logic for the the best selling item   */}
+}
+export async function getTotalRevenue() {
+    {/*Create Logic for getting the total revenue   */}
+}
